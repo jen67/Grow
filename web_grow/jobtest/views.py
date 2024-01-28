c8e54898adf1866f73fe46a6b609f7bf24a55916
Eniola011 <yettyagbalu@gmail.com>
2024-01-29 00:57:32 +0100

    index page

--- web_grow/jobtest/views.py
+++ web_grow/jobtest/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""
 
-# Create your views here.
+Views are functions or classes that handle
+HTTP requests and return HTTP responses.
+
+"""
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hi, you're at the job test index.")
