d066ed1745365b4cb92b9cda8f1d2ee1a1752287
Eniola011 <yettyagbalu@gmail.com>
2024-01-29 13:34:23 +0100

    created first question

--- web_grow/jobtest/models.py
+++ web_grow/jobtest/models.py
@@ -1,3 +1,37 @@
+"""
+
+Our source of information about grow data.
+
+"""
+
+
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    """Code Test the user will take."""
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration_minutes = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    """Questions asked"""
+    test_lang = [
+        ('python', 'Python'),
+        ('C', 'C' ),
+        ('javascript', 'Javascript'),
+    ]
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    text = models.TextField()
+    points = models.IntegerField()
+    code = models.TextField()
+    language = models.CharField(max_length=20, choices=test_lang, default='python')
+
+    def __str__(self):
+        return self.text
