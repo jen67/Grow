adba4007021d5ed8b7eac487048d57173d1dcc54
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-02-09 22:48:33 +0100

    update file

--- web_grow/jobtest-frontend/src/App.js
+++ web_grow/jobtest-frontend/src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import MyFigure from './components/MyFigure';
 import MySection from './components/MySection';
+import Hero from './components/Hero';
 
 
 function App() {
   return (
     <>
     <Navbar />
-    <main className='flex justify-between'>
+    <Hero />
+    {/* <main className='flex justify-between'>
       <MySection />
       <MyFigure />
-    </main>
+    </main> */}
     </>
   );
 }
