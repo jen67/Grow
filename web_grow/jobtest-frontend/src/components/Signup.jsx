d0ee76bfd17501907e492a513b9e349c53d941fd
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-02-10 23:26:20 +0100

    create new components

---
+++ web_grow/jobtest-frontend/src/components/Signup.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div className="w-full py-16 text-white">
+      <div className="max-w-[1240px] mx-auto text-center">
+        <div>
+          <p className="md:text-xl">
+            Join Grow now to start your journey towards interview success.{" "}
+            <br /> Sign up for early access and step confidently into your next
+            job interview!
+          </p>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Sign up</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
