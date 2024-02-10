3c1d2dd3e6de02837a496fe7f1d8237c943ffba3
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-02-10 23:24:27 +0100

    rename platform to product

---
+++ web_grow/jobtest-frontend/src/components/Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import personalized from '../assets/11.png'
+
+function Product() {
+  return (
+    <div className='w-full bg-white py-11 px-4'>
+        <header className='uppercase font-bold text-xl md:text-4xl sm:text-3xl text-center pb-4 '>why use our platform</header>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            <img className='w-[500px] mx-auto my-4' src={personalized} alt="/" />
+            <div className='flex flex-col justify-center'>
+                <h2 className='font-bold text-[#00df9a] text-xl mx-auto'>Personalized Interview Training</h2>
+                <p>Tailor your interview preparation with personalized training sessions, ensuring you focus on areas that matter most for your target job.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Product
