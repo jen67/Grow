cd397fedcc72212a34e0d9ee5709cca914f9ef59
jen67 <amakrigift2000@gmail.com>
2024-02-29 03:22:39 +0100

    added more pages

--- web_grow/jobtest-frontend/src/components/Footer.jsx
+++ web_grow/jobtest-frontend/src/components/Footer.jsx
@@ -1,30 +1,52 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-import Github from "../assets/Github1.png";
-import Twitter from "../assets/Twitter1.png";
-import Linkedin from "../assets/Linkedin1.png";
-import Facebook from "../assets/Facebook1.png";
-import Instagram from "../assets/Instagram1.png";
+import {
+  FaInstagram,
+  FaTwitter,
+  FaFacebookF,
+  FaGithub,
+  FaLinkedinIn,
+} from "react-icons/fa";
+// // import { Link } from "react-router-dom";
+// import Github from "../assets/Github1.png";
+// import Twitter from "../assets/Twitter1.png";
+// import Linkedin from "../assets/Linkedin1.png";
+// import Facebook from "../assets/Facebook1.png";
+// import Instagram from "../assets/Instagram1.png";
 import logo from "../assets/DarkThemeLogo.png";
 
 
 function Footer() {
   return (
     <div className="mx-auto py-16 px-6 md:px-10 lg:px-20 flex flex-col md:flex-row md:space-evenly items-center justify-center text-center gap-7 text-gray-300 w-full">
-      <div className="w-1/4">
+      <div className="md:w-1/4">
         <header className="w-full">
           <img src={logo} alt="logo" className="w-48 " />
         </header>
-        <div className="flex flex-row mt-8 gap-4" >
-          <img src={Instagram} alt="Instagram icon" className="cursor-pointer "></img>
-          <img src={Twitter} alt="Twitter icon" className="cursor-pointer "></img>
-          <img src={Facebook} alt="Facebook icon" className="cursor-pointer "></img>
-          <img src={Github} alt="Github icon" className="cursor-pointer "></img>
-          <img src={Linkedin} alt="Github icon" className="cursor-pointer "></img>
+        <div className="flex flex-row mt-8 gap-4">
+          <FaInstagram
+            className="cursor-pointer hover:scale-110 transition-transform hover:text-primary"
+            size={25}
+          />
+          <FaTwitter
+            className="cursor-pointer hover:scale-110 transition-transform hover:text-primary"
+            size={25}
+          />
+          <FaFacebookF
+            className="cursor-pointer hover:scale-110 transition-transform hover:text-primary"
+            size={20}
+          />
+          <FaGithub
+            className="cursor-pointer hover:scale-110 transition-transform hover:text-primary"
+            size={25}
+          />
+          <FaLinkedinIn
+            className="cursor-pointer hover:scale-110 transition-transform hover:text-primary"
+            size={25}
+          />
         </div>
       </div>
 
-      <div className="flex flex-wrap md:flex-row gap-8 md:justify-between items-center text-left font-roboto text-base w-3/4 md:text-center">
+      <div className="flex flex-wrap md:flex-row gap-8 md:justify-between items-center text-left font-roboto text-base md:w-3/4">
         <div>
           <h5 className="font-medium text-gray-400 hover:text-white transition-colors duration-200 cursor-pointer">
             Solutions
--- web_grow/jobtest-frontend/src/pages/AboutUs.jsx
+++ web_grow/jobtest-frontend/src/pages/AboutUs.jsx
@@ -1,16 +1,201 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import OurVission from "../assets/vision.jpg";
+import OurMission from "../assets/Mission.jpg";
+import Gift from "../assets/Gift.png";
+import Eniola from "../assets/Eniola.webp";
+import Justine from "../assets/Justine.png";
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function AboutUs() {
     return (
-      
-        <div>
+      <div>
         <Navbar />
+        <div className="py-32 text-white mx-auto px-4 bg-black flex flex-col items-center text-center justify-center w-full max-w-[900px] font-roboto">
+          <h1 className="md:text-[4rem] sm:text-5xl text-4xl font-bold md:py-6 font-inter ">
+            About Us
+          </h1>
+          <p className="text-base py-4 md:py-6 mt-4 px-4 md:max-w-[50vw] xl:max-w-[35vw] text-center">
+            Grow is a website dedicated to preparing individuals for
+            tech-related interviews. We provide comprehensive resources and
+            guidance to help candidates succeed in their job search and land
+            their dream roles in the tech industry.
+          </p>
+        </div>
+
+        <div className="bg-white font-roboto grid md:grid-cols-2">
+          <img
+            src={OurVission}
+            alt="Our Vision"
+            className="w-full h-full object-cover"
+          />
+          <div className="p-8 bg-whhite xl:px-28 flex flex-col justify-center">
+            <h2 className="text-3xl font-bold font-inter pb-4 text-left">
+              Our Vision
+            </h2>
+            <p className="text-black text-base xl:text-[1.1rem]">
+              Driven by our passion for excellence, we strive to exceed
+              expectations by consistently adapting to the evolving needs of our
+              community. With a focus on fostering diversity and empowerment, we
+              recognize and celebrate the unique strengths and contributions of
+              every individual, fostering a culture of belonging and support.
+              Our dedication to innovation pushes us to explore new horizons,
+              pioneering cutting-edge solutions that redefine industry standards
+              and inspire positive change.
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white font-roboto grid md:grid-cols-2">
+          <img
+            src={OurMission}
+            alt="Our Mission"
+            className="w-full h-full object-cover md:order-last"
+          />
+          <div className="p-8 bg-whhite xl:px-28 flex flex-col justify-center">
+            <h2 className="text-3xl font-bold font-inter pb-4 ">Our Mission</h2>
+            <p className="text-black text-base xl:text-[1.1rem]">
+              Our mission is to empower individuals to excel in the
+              ever-evolving landscape of technology interviews. We strive to
+              provide comprehensive resources and personalized support that
+              equip candidates with the confidence, skills, and knowledge needed
+              to succeed in their career aspirations. By fostering an
+              environment of continuous learning, collaboration, and innovation,
+              we aim to bridge the gap between ambition and achievement,
+              empowering every individual to unlock their full potential and
+              thrive in the tech industry.
+            </p>
+          </div>
+        </div>
+        <div className="bg-grey flex flex-col md:flex-row justify-around items-center md:justify-center md:flex-wrap  lg:flex-nowrap p-4 lg:py-24 lg:px-8 xl:px-24  xl:py-56 font-roboto">
+          <div className="bg-white shadow-lg rounded-lg p-6 m-4 flex flex-col items-center w-[300px] max-w-[600px] lg:w-[450px] h-[500px]">
+            <img
+              src={Eniola}
+              alt="Enioloa Agbalu"
+              className="rounded-full border-green-500 border-2 w-[150px] h-[150px]"
+            />
+            <h2 className="text-xl font-bold mt-4 font-inter">Enioloa Agbalu</h2>
+            <p className="text-center py-4">
+              Eniola is a dedicated backend developer with some years of
+              experience. She specializes in the use of python, C language, node
+              and many more. She Handled the backend for this project.
+            </p>
+            <p className="text-center font-semibold">Backend Developer</p>
+            <div className="flex justify-around mt-4 gap-3">
+              <a
+                href="github_link"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                <FaGithub size={24} />
+              </a>
+              <a
+                href="linkedin_link"
+                className="text-blue-600 hover:text-blue-900"
+              >
+                <FaLinkedin size={24} />
+              </a>
+              <a
+                href="twitter_link"
+                className="text-blue-400 hover:text-blue-700"
+              >
+                <FaTwitter size={24} />
+              </a>
+              <a
+                href="instagram_link"
+                className="text-pink-600 hover:text-pink-900"
+              >
+                <FaInstagram size={24} />
+              </a>
+            </div>
+          </div>
+
+          <div className="bg-white shadow-lg rounded-lg p-6 m-4 flex flex-col items-center w-[300px] max-w-[600px] lg:w-[450px] h-[500px]">
+            <img
+              src={Justine}
+              alt="Justine Ugwu"
+              className="rounded-full border-green-500 border-2 w-[150px] h-[150px]"
+            />
+            <h2 className="text-xl font-bold mt-4 font-inter">Justine Ugwu</h2>
+            <p className="text-center py-4">
+              justine is a dedicated frontend developer known for his passion
+              for creating captivating web experiences. With a keen eye for
+              detail and a commitment to excellence, he consistently delivers
+              high-quality results.
+            </p>
+            <p className="text-center font-semibold">Frontend Developer</p>
+            <div className="flex justify-around mt-4 gap-3">
+              <a
+                href="github_link"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                <FaGithub size={24} />
+              </a>
+              <a
+                href="linkedin_link"
+                className="text-blue-600 hover:text-blue-900"
+              >
+                <FaLinkedin size={24} />
+              </a>
+              <a
+                href="twitter_link"
+                className="text-blue-400 hover:text-blue-700"
+              >
+                <FaTwitter size={24} />
+              </a>
+              <a
+                href="instagram_link"
+                className="text-pink-600 hover:text-pink-900"
+              >
+                <FaInstagram size={24} />
+              </a>
+            </div>
+          </div>
+
+          <div className="bg-white shadow-lg rounded-lg p-6 m-4 flex flex-col items-center w-[300px] lg:w-[450px] max-w-[600px] h-[500px]">
+            <img
+              src={Gift}
+              alt="Gift Amachree"
+              className="rounded-full border-green-500 border-2 w-[150px] h-[150px]"
+            />
+            <h2 className="text-xl font-bold mt-4 font-inter">Gift Amachree</h2>
+            <p className="text-center py-4">
+              Gift is a passionate frontend developer and a life long learner.
+              She specializes in the use of html, css,javascript, nodejs, sass
+              etc. And she also does some designing with figma and canva
+            </p>
+            <p className="text-center font-semibold">Frontend Developer</p>
+            <div className="flex justify-around mt-4 gap-3">
+              <a
+                href="github_link"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                <FaGithub size={24} />
+              </a>
+              <a
+                href="linkedin_link"
+                className="text-blue-600 hover:text-blue-900"
+              >
+                <FaLinkedin size={24} />
+              </a>
+              <a
+                href="twitter_link"
+                className="text-blue-400 hover:text-blue-700"
+              >
+                <FaTwitter size={24} />
+              </a>
+              <a
+                href="instagram_link"
+                className="text-pink-600 hover:text-pink-900"
+              >
+                <FaInstagram size={24} />
+              </a>
+            </div>
+          </div>
+        </div>
         <Footer />
-      
-    </div>
-  );
+      </div>
+    );
 }
 
 export default AboutUs;
